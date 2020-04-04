38b3d876102f7e67046258140dea7bbd753af96a
tatawat22 <63144078+tatawat22@users.noreply.github.com>
2020-04-04 15:51:12 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = '+chCVZa4dTd2cVfil+L7rGbB6n/FG6hp/gMe/q7q7VD8kjP5u2C+0uN8H4DpQSihabKRAR/bIF/EYiUy1M5490Nt1A/Oos6JDcSFJiVcoer1IXKynsmkG0OTNgeaKfgjzPcngXUB37ynftz7oNsYGgdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
